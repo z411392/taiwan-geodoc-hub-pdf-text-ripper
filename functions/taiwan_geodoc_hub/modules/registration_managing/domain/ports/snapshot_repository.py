82e39a3ed81b8c1189dc1e84b7e16ab734eaaedb
z411392 <z411392@gmail.com>
2025-05-23 14:04:23 +0800

    initial commit

---
+++ functions/taiwan_geodoc_hub/modules/registration_managing/domain/ports/snapshot_repository.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from taiwan_geodoc_hub.modules.registration_managing.dtos.snapshot import Snapshot
+from google.cloud.firestore import Transaction
+
+
+class SnapshotRepository(ABC):
+    @abstractmethod
+    def exists(self, snapshot_id: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(
+        self,
+        snapshot_id: str,
+        snapshot: Snapshot,
+        /,
+        transaction: Transaction,
+    ):
+        raise NotImplementedError
---
+++ functions/tests/e2e/test_registration_managing.py
@@ -0,0 +1,35 @@
+import pytest
+from injector import Injector
+from taiwan_geodoc_hub.http import app
+from flask import g
+from flask.testing import FlaskClient
+from os import getenv
+from json import dumps
+
+
+@pytest.mark.skip(reason="")
+class TestRegistrationManaging:
+    @pytest.fixture(scope="module")
+    def client(self, injector: Injector):
+        with app.test_client() as client:
+            with app.app_context():
+                g.injector = injector.create_child_injector()
+                yield client
+
+    @pytest.mark.describe("要能夠上傳謄本並解析 PDF 中的文字")
+    def test_ripping_text(
+        self,
+        client: FlaskClient,
+    ):
+        client.set_cookie("SESSION", getenv("SESSION_COOKIE"))
+        response = client.post(
+            "/",
+            data=dumps(
+                dict(
+                    tenant_id=getenv("TENANT_ID"),
+                    content=getenv("PDF_SAMPLE"),
+                )
+            ),
+            content_type="application/json",
+        )
+        assert response.status_code == 200
